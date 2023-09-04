f6cec1fc2b2e629309feeb743352391ed9c727ab
Violin Iliev <violin.developer@gmail.com>
2023-09-03 18:12:22 -0700

    upgrade to Godot 4.1.1

--- Themes/TestBed.gd
+++ Themes/TestBed.gd
@@ -1,16 +1,14 @@
 extends Node
 
-onready var dialog = $GUI/ControlsDialog
-onready var acceptDialog = $GUI/AcceptDialog
-onready var confirmDialog = $GUI/ConfirmationDialog
-onready var fileDialog = $GUI/FileDialog
-onready var themesCtrl = $GUI/TaskBar/Themes
-onready var gui = $GUI
+@onready var themesCtrl = $GUI/TaskBar/Themes
+@onready var gui = $GUI
+@onready var controlsDialog = preload ("res://Themes/TestDialog.tscn")
 
 var themes = {
 	0: ThemeItem.new("Default", "."),
 	1: ThemeItem.new("Classic 3.11", "res://Themes/Classic311/Classic311.tres"),
-	2: ThemeItem.new("Classic 95", "res://Themes/Classic95/Classic95.tres")
+	2: ThemeItem.new("Classic 95", "res://Themes/Classic95/Classic95.tres"),
+	3: ThemeItem.new("Modern 11 (WIP)", "res://Themes/Modern11/Modern11.tres")
 }
 
 func _ready():
@@ -18,17 +16,23 @@ func _ready():
 		var theme = themes[key]
 		themesCtrl.add_item(theme.name, key)
 	changeTheme(2) # pre-select Classic 95
+	#changeTheme(3) # pre-select Modern 11
 
 func changeTheme(index):
 	themesCtrl.select(index) 
 	_on_Themes_item_selected(index)
 
 func _on_Button_button_up():
-	#dialog.visible = true
-	var new_dialog = dialog.duplicate()
-	gui.add_child(new_dialog)
-	new_dialog.visible = true
-
+	var window = Window.new()
+	window.title = "Controls Demo"	
+	window.initial_position = Window.WINDOW_INITIAL_POSITION_CENTER_MAIN_WINDOW_SCREEN
+	window.visible = true
+	window.size.x = 595
+	window.size.y = 380	
+	window.close_requested.connect(window.queue_free)
+	window.add_child(controlsDialog.instantiate())
+	
+	gui.add_child(window)
 
 class ThemeItem:
 	var name = ""
@@ -47,18 +51,38 @@ func _on_Themes_item_selected(index):
 		gui.theme = load(themeResource)
 
 func _on_Alert_button_up():
-	putOnTop(acceptDialog)
-	acceptDialog.visible = true
+	var dialog = AcceptDialog.new()
+	dialog.title = "Alert"
+	dialog.initial_position = Window.WINDOW_INITIAL_POSITION_CENTER_MAIN_WINDOW_SCREEN
+	dialog.size = Vector2i(200, 100)
+	dialog.dialog_hide_on_ok = true
+	dialog.dialog_close_on_escape = true
+	dialog.dialog_text = "Hello World"
+	dialog.visible = true
+	gui.add_child(dialog)
 
 
 func _on_Confirm_button_up():
-	putOnTop(confirmDialog)
-	confirmDialog.visible = true
+	var dialog = ConfirmationDialog.new()
+	dialog.title = "Please confirm"
+	dialog.initial_position = Window.WINDOW_INITIAL_POSITION_CENTER_MAIN_WINDOW_SCREEN
+	dialog.size = Vector2i(200, 100)
+	dialog.dialog_hide_on_ok = true
+	dialog.dialog_close_on_escape = true
+	dialog.dialog_text = "This is a dialog. Ok?"
+	dialog.visible = true
+	gui.add_child(dialog)
 
 
 func _on_File_button_up():
-	putOnTop(fileDialog)
-	fileDialog.visible = true
+	var dialog = FileDialog.new()
+	dialog.title = "Save a file"
+	dialog.initial_position = Window.WINDOW_INITIAL_POSITION_CENTER_MAIN_WINDOW_SCREEN
+	dialog.size = Vector2i(720, 450)
+	dialog.dialog_hide_on_ok = true
+	dialog.dialog_close_on_escape = true
+	dialog.visible = true
+	gui.add_child(dialog)
 	
 func putOnTop(control):
 	var parent = control.get_parent()
